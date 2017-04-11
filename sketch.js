a02d635e5e3d1205113632c4c17f3960a0eee102
JustHardCodeIt <ultimatedestoyer122@gmail.com>
2017-04-10 21:26:15 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,32 @@
 
-var angle = 0;
-var slider;
-var level;
-var currentStoke;
-
-
-
+var Mic;
+var amp;
 function setup() {
-  createCanvas(400, 400);
-  slider = createSlider(0, TWO_PI, PI/4, 0.01);
- 
+	Mic = new p5.AudioIn();
+	Mic.start();
+	createCanvas(710, 400);
+	amp = new p5.FFT();
+	amp.setInput(Mic);
 }
 
 function draw() {
-  background(51);
-  angle = slider.value();
-  var len = 100;
-  translate(200, height)
-  currentStoke = 255;
-  stroke(currentStoke);
-  branch(len);
-  
+	var spectrum = amp.analyze();
+  	background(0);
+  	beginShape();
+  	noFill(0);
+  	stroke(255);
+
+  	var half = height/2;
+
+  	for(var x = 0; x < spectrum.length; x++){
+  		 if(spectrum[x] < 0) spectrum[x] = 1;
+  		 vertex(x, half + map(spectrum[x], 0, 255, 0, height) );
+  		 vertex(x, half - map(spectrum[x], 0, 255, 0, height) );
+
+  	}
+  	endShape();
+
+  	
+
+  	
 }
-
-function branch(len){
-	line(0, 0, 0, -len);
-	translate(0, -len);
-	level++;
-	stroke(currentStoke/level);	
-  	if(len > 4){
-  		push();
-    	rotate(angle);
-    	branch(len * 0.67);
-    	pop();
-    	rotate(-angle)
-    	branch(len * .67);
-  }
-}
-
-
